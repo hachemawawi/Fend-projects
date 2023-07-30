b3c7054a3ed639c4ca4bb1243bd54cb223204012
Hachem Awawi <62650081+hachemawawi@users.noreply.github.com>
2023-07-30 15:25:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 // space, scary, military, romantic, cowboy, fantasy, superhero
 favouriteMovieGenre("space")
 // watermelon, tomato, banana, orange, avocado, blueberry
-favouriteFruit("blueberry")
+favouriteFruit("orange")
 
 // light, dark
 favouriteMode("dark")
